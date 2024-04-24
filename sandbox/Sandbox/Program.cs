dd3eb522d10770b13a4d21b77d91b3f0d9fb3970
darrencc1 <darrencc@byui.edu>
2024-04-24 15:45:01 -0600

    Code

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -8,9 +8,17 @@ class Program
         int x;
         x = 5;
         Console.WriteLine(x);
-
+        int y;
+        y = 1;
         Console.Write("What is your favorite color? ");
         string color = Console.ReadLine();
+
+        if (x > y)
+        {
+            Console.WriteLine("greater");
+        }
+        string school = "BYU-Idaho";
+        Console.WriteLine($"I am studying at {school}.");
     }
 }
 
