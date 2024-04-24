75b50cca460ff29dcacf93ae43b70b8efdc152bf
darrencc1 <darrencc@byui.edu>
2024-04-24 16:15:21 -0600

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade_text = Console.ReadLine();
+        float grade = float.Parse(grade_text);
+        
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is an A!");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade is an B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is a C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is a D");
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine("You are failing this class!");
+        }
     }
 }
