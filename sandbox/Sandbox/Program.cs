ec33e16c1f1136f075cc595bc428a44510052883
darrencc1 <darrencc@byui.edu>
2024-04-24 15:15:39 -0600

    commit I guess

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hello World!");
+        int x;
+        x = 5;
+        Console.WriteLine(x);
+
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
     }
 }
+
