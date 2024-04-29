194d9a63107891b94b889833ea34408e7c118e7f
darrencc1 <darrencc@byui.edu>
2024-04-28 18:06:43 -0600

    Basics, Need to apply while loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+      Console.Write("What is the magic number? ");
+      int magicNumber = int.Parse(Console.ReadLine());
+
+      Console.Write("What is the magic number? ");
+      int guess = int.Parse(Console.ReadLine());
+
+      if (guess > magicNumber)
+      {
+        Console.WriteLine("Please guess a lower number");
+      }
+      else if (guess < magicNumber)
+      {
+        Console.WriteLine("Please Guess a higher number");
+      } 
+      else if (guess == magicNumber)
+      {
+        Console.WriteLine("Congratulations you guessed it!");
+      }       
     }
 }
