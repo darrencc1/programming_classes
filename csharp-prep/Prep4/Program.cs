6e86c866509e4407c63aa6b24b83ee4d9388a4ff
darrencc1 <darrencc@byui.edu>
2024-04-29 16:19:53 -0600

    psuh

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,31 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = 1;
+        int sum = 0;
+        Console.Write("Enter a list of numbers, type 0 when finished.");
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            
+            number = int.Parse(Console.ReadLine());
+            sum += number;
+            if (number == 0)
+            {
+                break;
+            }
+            numbers.Add(number);
+        }
+        Console.WriteLine("Entered numbers:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+        Console.WriteLine($"Sum: {sum}");
+    
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,21 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
-        int x;
-        x = 5;
-        Console.WriteLine(x);
-        int y;
-        y = 1;
-        Console.Write("What is your favorite color? ");
-        string color = Console.ReadLine();
+      Console.WriteLine("Counting from 1 - 10:");
+      for (int i = 1; i <= 10; i++)
+      {
+        // i--;
+        // i -= 1;
+        // i = i - 1;
+        Console.WriteLine(i);
+      }
 
-        if (x > y)
-        {
-            Console.WriteLine("greater");
-        }
-        string school = "BYU-Idaho";
-        Console.WriteLine($"I am studying at {school}.");
+
     }
 }
 
