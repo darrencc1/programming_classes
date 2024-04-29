9ebfda738f85c4eb633730750b1daa38d93319ab
darrencc1 <darrencc@byui.edu>
2024-04-29 15:38:56 -0600

    Random Number Guess Complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,23 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-      Console.Write("What is the magic number? ");
-      int magicNumber = int.Parse(Console.ReadLine());
-
-      Console.Write("What is the magic number? ");
-      int guess = int.Parse(Console.ReadLine());
-
-      if (guess > magicNumber)
-      {
-        Console.WriteLine("Please guess a lower number");
-      }
-      else if (guess < magicNumber)
-      {
-        Console.WriteLine("Please Guess a higher number");
-      } 
-      else if (guess == magicNumber)
-      {
-        Console.WriteLine("Congratulations you guessed it!");
-      }       
+      Random randomGenerator = new Random();
+      int magicNumber = randomGenerator.Next(1, 11);
+      
+      int guessCount = 0;
+      int guess = 0;
+      while (guess != magicNumber) 
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            guessCount = guessCount + 1;
+        if (guess > magicNumber)
+        {
+            Console.WriteLine("Please guess a lower number");
+        }
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("Please Guess a higher number");
+        } 
+        else if (guess == magicNumber)
+        {
+            Console.WriteLine("Congratulations you guessed it!");
+            Console.WriteLine($"It took you {guessCount} times to guess the number.");
+        }  
+       }      
     }
 }
