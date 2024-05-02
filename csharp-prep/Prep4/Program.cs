6f30c8ab08917bce0fac103eb4324a6cef63b8e3
darrencc1 <darrencc@byui.edu>
2024-05-01 21:16:18 -0600

    push

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -7,23 +7,41 @@ class Program
         List<int> numbers = new List<int>();
         int number = 1;
         int sum = 0;
-        Console.Write("Enter a list of numbers, type 0 when finished.");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
         while (number != 0)
         {
             Console.Write("Enter a number: ");
-            
             number = int.Parse(Console.ReadLine());
-            sum += number;
-            if (number == 0)
+            
+            if (number != 0)
             {
-                break;
+                numbers.Add(number);
+                sum += number;
             }
-            numbers.Add(number);
         }
-        Console.WriteLine("Entered numbers:");
-        foreach (int num in numbers)
+
+        if (numbers.Count > 0)
         {
-            Console.WriteLine(num);
+            double average = (double)sum / numbers.Count;
+            int max = numbers[0];
+
+            foreach (int num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+            }
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
         }
-        Console.WriteLine($"Sum: {sum}");9u999
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
+    }
 }
