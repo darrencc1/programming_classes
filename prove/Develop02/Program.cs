1272b7ae7354a10be17ea31f96f593267f9c7f95
darrencc1 <darrencc@byui.edu>
2024-05-15 12:40:23 -0600

    send it

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+            int userChoice = 1;
+            while (userChoice != 0)
+        {
+        Console.WriteLine("1. Write New Entry");
+        Console.WriteLine("2. Load Entry");
+        Console.WriteLine("3. Save Journal");
+        Console.WriteLine("4. Display Journal");
+        Console.WriteLine("Press 0 to end program");
+        Console.Write("Please Pick a Number Option: ");
+        userChoice = int.Parse(Console.ReadLine());  
+        }
+       
+    
     }
 }
---
+++ prove/Develop02/entry.cs
@@ -0,0 +1,9 @@
+using System;
+
+class entry
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Develop02 World!");
+    }
+}
